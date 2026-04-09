1ba0b2f7c5415d93ccd206e93974e52c5db24596
mateodiseno04-beep <mateodiseno04@gmail.com>
2026-04-09 19:59:09 -0300

    Create airtable.js

---
+++ netlify/functions/airtable.js
@@ -0,0 +1,43 @@
+const https = require('https');
+
+exports.handler = async (event) => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+    };
+
+    if (event.httpMethod === 'OPTIONS') return { statusCode: 200, headers, body: '' };
+
+    try {
+        const { table, method, id, body } = JSON.parse(event.body || '{}');
+        const TOKEN = process.env.AIRTABLE_TOKEN;
+        const BASE_ID = process.env.AIRTABLE_BASE_ID;
+
+        const path = `/v0/${BASE_ID}/${table}${id ? `/${id}` : ''}`;
+        
+        return new Promise((resolve) => {
+            const options = {
+                hostname: 'api.airtable.com',
+                path: path,
+                method: method || 'GET',
+                headers: {
+                    'Authorization': `Bearer ${TOKEN}`,
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            const req = https.request(options, (res) => {
+                let data = '';
+                res.on('data', (d) => data += d);
+                res.on('end', () => resolve({ statusCode: res.statusCode, headers, body: data }));
+            });
+
+            req.on('error', (e) => resolve({ statusCode: 500, headers, body: e.message }));
+            if (body) req.write(JSON.stringify(body));
+            req.end();
+        });
+    } catch (err) {
+        return { statusCode: 500, headers, body: JSON.stringify({ error: err.message }) };
+    }
+};
